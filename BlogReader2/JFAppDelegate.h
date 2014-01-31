a6c94fd579b82a1ff5654f6819f98b4cbe64a0c3
Jason Fisch <Jason@jasons-air.home>
2014-01-30 22:23:48 -0500

    Initial Commit

---
+++ BlogReader2/JFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JFAppDelegate.h
+//  BlogReader2
+//
+//  Created by Jason Fisch on 1/30/14.
+//  Copyright (c) 2014 Jason Fisch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlogReader2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlogReader2
+//
+//  Created by Jason Fisch on 1/30/14.
+//  Copyright (c) 2014 Jason Fisch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JFAppDelegate class]));
+    }
+}
